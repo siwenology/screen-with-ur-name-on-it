ac29d016f661170108d6c5bf3edfed8576ee61eb
AnjaliY <yeh_si_wen_2029@mgs.sch.edu.sg>
2026-07-04 14:22:53 +0800

    Initial Commit

---
+++ screen with ur name on it/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  screen with ur name on it
+//
+//  Created by Anjali Yeh on 4/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ screen with ur name on it/screen_with_ur_name_on_itApp.swift
@@ -0,0 +1,17 @@
+//
+//  screen_with_ur_name_on_itApp.swift
+//  screen with ur name on it
+//
+//  Created by Anjali Yeh on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct screen_with_ur_name_on_itApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
