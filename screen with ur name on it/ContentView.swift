ee37ebfbe1638197e22dd6d94932255a5056db5f
Gabriel <gabrielthamls12@gmail.com>
2026-07-04 14:46:27 +0800

    added name and bg color

--- screen with ur name on it/ContentView.swift
+++ screen with ur name on it/ContentView.swift
@@ -9,16 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color.purple
+                .ignoresSafeArea()
+            VStack{
+                Text("Gab is real")
+                    .font(.largeTitle)
+                    .foregroundStyle(.green)
+               
+                Text("HALLOOO")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+            }
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
-
-#Preview {
-    ContentView()
-}
+        
+        #Preview {
+            ContentView()
+    }
